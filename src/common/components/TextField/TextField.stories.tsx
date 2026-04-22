a85b6581439623a5d36e8e482b728fc03c765d01
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2026-04-22 23:39:06 +0300

    fix storybook import

--- src/common/components/TextField/TextField.stories.tsx
+++ src/common/components/TextField/TextField.stories.tsx
@@ -1,40 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { TextField } from './TextField';
+import type { Meta, StoryObj } from "@storybook/nextjs-vite"
+import { TextField } from "./TextField"
 
 const meta = {
-  title: 'Components/TextField',
+  title: "Components/TextField",
   component: TextField,
-  tags: ['autodocs'],
-} satisfies Meta<typeof TextField>;
+  tags: ["autodocs"],
+} satisfies Meta<typeof TextField>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 // Обычный инпут
 export const Default: Story = {
   args: {
-    label: 'Email',
-    placeholder: 'Enter your email...',
+    label: "Email",
+    placeholder: "Enter your email...",
   },
-};
+}
 
 // Инпут с ошибкой
 export const WithError: Story = {
   args: {
-    label: 'Password',
-    errorMessage: 'Error text',
-    type: 'password',
-    value: 'wrong-password',
+    label: "Password",
+    errorMessage: "Error text",
+    type: "password",
+    value: "wrong-password",
   },
-};
+}
 
 // Инпут поиска (с иконкой)
 export const Search: Story = {
   args: {
-    placeholder: 'Search...',
-    type: 'search',
+    placeholder: "Search...",
+    type: "search",
     // Здесь можно передать иконку, когда добавишь их в проект
     iconStart: null,
     iconEnd: null,
   },
-};
+}
