067691630ef894731a77a5d48dff09055db5b8df
molnia_grom <mgrom250@gmail.com>
2026-04-23 18:18:18 +0300

    IN-38 feat: init task

---
+++ src/common/components/ContentCards/ContentCards.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const ContentCards = () => {
+  return <div>ContentCards</div>
+}
--- src/common/components/Page/Page.tsx
+++ src/common/components/Page/Page.tsx
@@ -1,5 +1,14 @@
 import "./page.css"
+import { Header } from "@/common/components/Header/Header"
+import { RegisteredUsers } from "@/common/components/RegisteredUsers/RegisteredUsers"
+import { ContentCards } from "@/common/components/ContentCards/ContentCards"
 
 export const Page = () => {
-  return <h1>StarLions Frontend Project</h1>
+  return (
+    <div>
+      <Header />
+      <RegisteredUsers />
+      <ContentCards />
+    </div>
+  )
 }
