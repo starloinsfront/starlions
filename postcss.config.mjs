42d1c960d7b862ebb6d990b642c0679cf30c0082
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2026-04-23 22:04:29 +0300

    add --webpack

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,10 @@
-// import postcssCustomMedia from "postcss-custom-media"
-// import autoprefixer from "autoprefixer"
+import postcssCustomMedia from "postcss-custom-media"
+import autoprefixer from "autoprefixer"
 
 const postcssConfig = {
   plugins: {
-    "postcss-custom-media": {},
-    autoprefixer: {},
+    "postcss-custom-media": postcssCustomMedia(),
+    autoprefixer: autoprefixer(),
   },
 }
 
