2b6bc9a7f5f01e32608fda443006221d14ba45f5
molnia_grom <mgrom250@gmail.com>
2026-04-27 12:07:20 +0300

    IN-48 fix: resolve page rendering issues and refine content layout

--- src/common/components/Page/Page.tsx
+++ src/common/components/Page/Page.tsx
@@ -1,5 +1,16 @@
 import "./page.css"
+import { ContentCards } from "@/common/components/ContentCards/ContentCards"
+import { RegisteredUsers } from "@/common/components/RegisteredUsers/RegisteredUsers"
+import { Header } from "../Header/Header"
 
 export const Page = () => {
-  return <h1>StarLions Frontend Project</h1>
+  return (
+    <div>
+      <Header />
+      <div className="page-content-container">
+        <RegisteredUsers />
+        <ContentCards />
+      </div>
+    </div>
+  )
 }
