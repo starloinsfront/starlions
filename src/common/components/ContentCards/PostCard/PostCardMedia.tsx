8819631c4e73df1d6946f616524758b36807fdc5
molnia_grom <mgrom250@gmail.com>
2026-04-28 21:37:51 +0300

    IN-48 refactor: extract shared carousel component

--- src/common/components/ContentCards/PostCard/PostCardMedia.tsx
+++ src/common/components/ContentCards/PostCard/PostCardMedia.tsx
@@ -1,71 +1,32 @@
-import Link from "next/link"
-import { type CSSProperties } from "react"
-import { Icon } from "@/common/components/Icon/Icon"
+import { Carousel } from "@/common/components/Carousel/Carousel"
 import s from "../ContentCards.module.css"
 import { PostImage } from "../PostImageType"
 
 type Props = {
-  activeIndex: number
-  goToSlide: (index: number) => void
   images: PostImage[]
   postId: number
-  showNext: () => void
-  showPrev: () => void
 }
 
-export const PostCardMedia = ({
-  activeIndex,
-  goToSlide,
-  images,
-  postId,
-  showNext,
-  showPrev,
-}: Props) => {
+export const PostCardMedia = ({ images, postId }: Props) => {
   return (
-    <div className={s.media}>
-      <Link
-        aria-label={`Open post ${postId}: ${images[activeIndex].label}`}
-        className={s.slide}
-        href={`/post/${postId}`}
-        style={{ "--slide-background": images[activeIndex].background } as CSSProperties}
-      >
-        <span className={s.slideLabel}>{images[activeIndex].label}</span>
-      </Link>
-
-      {images.length > 1 && (
-        <>
-          <button
-            aria-label="Previous image"
-            className={`${s.navButton} ${s.navPrev}`}
-            onClick={showPrev}
-            type="button"
-          >
-            <Icon className={s.navIconLeft} height={27} name="arrowIosDownOutline" width={27} />
-          </button>
-
-          <button
-            aria-label="Next image"
-            className={`${s.navButton} ${s.navNext}`}
-            onClick={showNext}
-            type="button"
-          >
-            <Icon className={s.navIconRight} height={27} name="arrowIosDownOutline" width={27} />
-          </button>
-
-          <div className={s.dots}>
-            {images.map((image, index) => (
-              <button
-                aria-current={activeIndex === index}
-                aria-label={`Open ${image.label}`}
-                className={s.dot}
-                key={image.label}
-                onClick={() => goToSlide(index)}
-                type="button"
-              />
-            ))}
-          </div>
-        </>
-      )}
-    </div>
+    <Carousel
+      classNames={{
+        dot: s.dot,
+        dots: s.dots,
+        navButton: s.navButton,
+        navNext: s.navNext,
+        navPrev: s.navPrev,
+        root: s.media,
+        slide: s.slide,
+      }}
+      labelClassName={s.slideLabel}
+      slides={images.map((image) => ({
+        background: image.background,
+        href: `/post/${postId}`,
+        id: image.label,
+        label: image.label,
+      }))}
+      variant="card"
+    />
   )
 }
