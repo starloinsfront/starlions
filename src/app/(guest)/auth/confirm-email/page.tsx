65a4955a2a0ecf22550118e65eea0f3915ddcd88
ropotigor18 <ropotigor18@gmail.com>
2026-05-08 12:41:31 +0300

    styles confirm page

--- src/app/(guest)/auth/confirm-email/page.tsx
+++ src/app/(guest)/auth/confirm-email/page.tsx
@@ -2,10 +2,11 @@
 
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
-
 import s from "./ConfirmEmail.module.css"
 import { useRegistrationConfirmation } from "@/features/auth/api/useRegistrationConfirmation"
 import { Button } from "@/common/components/Button/Button"
+import Image from "next/image"
+import confirmImage from "@/assets/storybook/bro.png"
 
 export default function ConfirmEmailPage() {
   const searchParams = useSearchParams()
@@ -26,11 +27,12 @@ export default function ConfirmEmailPage() {
 
   return (
     <div className={s.container}>
-      <h1>Congratulations</h1>
-      <p>Your email has been confirmed</p>
+      <h1 className={s.titlePage}>Congratulations</h1>
+      <p className={s.subTitle}>Your email has been confirmed</p>
       <Button onClick={() => router.push("/login")} className={s.btn}>
         Sign In
       </Button>
+      <Image src={confirmImage} alt={"Confirm email"} className={s.confirmImage} />
     </div>
   )
 }
