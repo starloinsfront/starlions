7d1f5a305df7fb951c0110b9d28649a306b98195
Anna Babich <4773737@gmail.com>
2026-05-20 20:56:05 -0400

    IN-43 refactor: move logout logic from Sidebar into Logout component

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -8,23 +8,10 @@ import { Icon } from "@/common/components/Icon/Icon"
 import { NavLink } from "./NavLink/NavLink"
 import { sidebarSections } from "./sidebar.data"
 import styles from "./Sidebar.module.css"
-import { useState } from "react"
-import { Modal } from "@/common/components/Modal/Modal"
-import { Button } from "@/common/components/Button/Button"
-import { useLogoutMutation } from "@/features/auth/api/useLogoutMutation"
-import { useMe } from "@/features/auth/api/useMe"
+
 
 export const Sidebar = () => {
   const { main, secondary } = sidebarSections
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const mutation = useLogoutMutation()
-
-  const { data } = useMe()
-
-  const handleLogout = () => {
-    mutation.mutate()
-    setIsModalOpen(false)
-  }
 
 
   return (
@@ -59,42 +46,9 @@ export const Sidebar = () => {
       </nav>
 
       <div className={clsx(styles.logoutSection, styles.desktopOnly)}>
-        <Logout onClick={() => setIsModalOpen(true)} />
+        <Logout />
       </div>
-      <Modal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        modalTitle={"Log Out"}
-        size={"sm"}
-      >
-        <p>Are you really want to log out of your account {data?.email}?</p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            gap: "24px",
-            marginTop: "30px",
-          }}
-        >
-          <Button
-            onClick={handleLogout}
-            variant={"outline"}
-            style={{
-              width: "96px",
-            }}
-          >
-            Yes
-          </Button>
-          <Button
-            onClick={() => setIsModalOpen(false)}
-            style={{
-              width: "96px",
-            }}
-          >
-            No
-          </Button>
-        </div>
-      </Modal>
+
     </aside>
   )
 }
